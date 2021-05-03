9c86db6466ea9528e503e68afd069645c5c5d8d2
Nguyen Chanh Bao <nguyenchanhbao@pop-os.localdomain>
2021-05-03 12:48:30 +0700

    uibuild

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 const morgan = require("morgan");
+const cors = require("cors");
 const express = require("express");
+const router = require("./router");
 const app = express();
 
-const persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Dan Abramov",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Marry Poppendick",
-    number: "39-23-6423122",
-  },
-  {
-    id: 4,
-    name: "Ada Lovelace",
-    number: "12-43-234345",
-  },
-];
-
+app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :json")
@@ -33,56 +14,7 @@ app.use(
 
 morgan.token("json", (req, res) => JSON.stringify(req.body));
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
-});
-
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((item) => item.id === id);
-
-  if (!person) res.status(404);
-
-  res.json(person);
-});
-
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((item) => item.id === id);
-
-  if (person) {
-    // persons = persons.filter((elem) => elem.id !== id);
-    res.status(200).end();
-  }
-
-  res.status(404).end();
-});
-
-app.post("/api/persons", (req, res) => {
-  const id = Math.floor(Math.random(10, 1000));
-  const person = persons.find((item) => item.name === req.body.name);
-
-  if (!req.body.name || !req.body.number) {
-    res.status(406).json({ error: "Name or number cant be null" });
-  }
-
-  if (person) {
-    res
-      .status(406)
-      .json({ error: "The name is already exists in the phonebook" });
-  }
-
-  res.send({ ...req.body, id });
-});
-
-app.get("/info", (req, res) => {
-  const number = persons.length;
-  const date = new Date();
-
-  const infoStr = `Phonebook has info for ${number} people\n${date}`;
-
-  res.send(infoStr);
-});
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running at PORT: ${PORT}`));
---
+++ router.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const persons = require("./persons.json");
+
+router.get("/persons", (req, res) => res.json(persons));
+
+router.get("/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((item) => item.id === id);
+
+  if (!person) res.status(404).end();
+
+  res.json(person);
+});
+
+router.get("/info", (req, res) => {
+  const number = persons.length;
+  const date = new Date();
+
+  const infoStr = `Phonebook has info for ${number} people\n${date}`;
+
+  res.send(infoStr);
+});
+
+router.post("/persons", (req, res) => {
+  const id = Math.floor(Math.random(10, 1000));
+  const checkDuplicatePerson = persons.find(
+    (item) => item.name === req.body.name
+  );
+
+  if (!req.body.name || !req.body.number) {
+    res.status(406).json({ error: "Name or number cant be null" });
+  }
+
+  if (checkDuplicatePerson) {
+    res
+      .status(406)
+      .json({ error: "The name is already exists in the phonebook" });
+  }
+
+  const person = { ...req.body, id };
+  persons.push(person);
+  res.json(person);
+});
+
+router.put("/persons/:id", (req, res) => {
+  const person = persons.find((item) => item.name === req.body.name);
+  person.number = req.body.number;
+
+  res.json(person);
+});
+
+router.delete("/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((item) => item.id === id);
+
+  if (person) {
+    // persons = persons.filter((elem) => elem.id !== id);
+    res.status(200).end();
+  } else res.status(404).end();
+});
+
+module.exports = router;
